f1c76844c96b27a11d7c3f0ef16f32380ddaa43e
Игорь <vaskinin97@gmail.com>
2024-07-18 02:53:15 +0400

    dockerfile and reqs.txt

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import Depends, FastAPI
 from contextlib import asynccontextmanager
-from sql_app.database import create_tables, drop_tables
+from db.database import create_tables, drop_tables
 from router import router as refcodes_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await drop_tables()
-    print("Drop tables")
+    #await drop_tables()
+    #print("Drop tables")
     await create_tables()
     print("Create tables")
     yield
