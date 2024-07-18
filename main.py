1b31c1a245f1aae6fd2e4fe8e92ea35a1eff2238
Игорь <vaskinin97@gmail.com>
2024-07-18 20:18:59 +0400

    dont need create_tables bc alembic

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from db.db_helper import create_tables, drop_tables
 from referral_codes.router import router as refcodes_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    #await drop_tables()
-    #print("Drop tables")
-    await create_tables()
-    print("Create tables")
     yield
-    print("Closing connection")
 
 app = FastAPI(lifespan=lifespan, title="Referral Codes API", )
 app.include_router(refcodes_router)
