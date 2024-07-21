592be48570f39ebab8e3b5e55eaf5c380e91df15
Игорь <vaskinin97@gmail.com>
2024-07-21 20:00:58 +0400

    delete user refcode

--- users/router.py
+++ users/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from db.models import User
 from auth.fastapi_users import current_user
+from referral_codes.dependencies import refcode_by_id
 from users.repository import UserRepository
 from referral_codes.repository import RefCodeRepository
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -32,4 +33,9 @@ async def create_user_refcode(session: AsyncSession = Depends(db_helper.session_
         raise HTTPException(status_code=400, detail=str(e))
     return {"refcode": code_schema}
 
-
+@router.delete("/", status_code=204)
+async def delete_user_code(session: AsyncSession = Depends(db_helper.session_dependency), 
+                           user: User = Depends(current_user)
+                            ) -> None:
+    code = await UserRepository.get_user_refcode(session=session, user_id= user.id)
+    await RefCodeRepository.delete_code(session=session, code=code)
