abf62b16cf2c91afb08e8eccd0b696dc17549788
Игорь <vaskinin97@gmail.com>
2024-08-08 19:47:54 +0400

    just a reminder

--- db/models.py
+++ db/models.py
@@ -3,6 +3,14 @@ from sqlalchemy import Boolean, ForeignKey, Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from datetime import datetime
 
+"""
+Note that the ORM’s “delete” and “delete-orphan” behavior applies only to the use of the Session.delete()
+method to mark individual ORM instances for deletion within the unit of work process.
+It does not apply to “bulk” deletes, which would be emitted using the delete() construct
+as illustrated at ORM UPDATE and DELETE with Custom WHERE Criteria.
+See Important Notes and Caveats for ORM-Enabled Update and Delete for additional background.
+"""
+
 
 class Base(DeclarativeBase):
     __abstract__ = True
@@ -36,7 +44,3 @@ class ReferallCode(Base):
         ForeignKey("users.id", ondelete="CASCADE"), unique=True, nullable=False
     )
     user: Mapped["User"] = relationship("User", back_populates="referral_code")
-
-    def __str__(self):
-        string = f"code: {self.code}, created_date: {self.created_date}, expiration_date: {self.expiration_date}, user_id: {self.user_id}"
-        return string
